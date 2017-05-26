d3efea14220922163f581b629145f73ee6d89025
jollion <ollion@mnhn.fr>
2017-05-26 08:59:54 +0200

    revoir manual correction

--- src/main/java/plugins/plugins/trackers/bacteriaInMicrochannelTracker/BacteriaClosedMicrochannelTrackerLocalCorrections.java
+++ src/main/java/plugins/plugins/trackers/bacteriaInMicrochannelTracker/BacteriaClosedMicrochannelTrackerLocalCorrections.java
@@ -627,6 +627,7 @@ public class BacteriaClosedMicrochannelTrackerLocalCorrections implements Tracke
             }
             //logger.debug("get object @ {}, size: {}", timePoint, populations[timePoint].size());
             createAttributes(timePoint);
+            Collections.sort(populations[timePoint], (o1, o2)->Integer.compare(o1.getBounds().getyMin(), o2.getBounds().getyMin()));
         }
         return populations[timePoint];
     }
