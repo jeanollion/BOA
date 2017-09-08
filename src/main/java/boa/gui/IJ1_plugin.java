9684d06a99f0c05a2d54367c372f4dc83c1fa06f
jeanollion <ollion@mnhn.fr>
2017-09-08 16:42:05 +0200

    lookAndFeel management for ImageJ

--- src/main/java/boa/gui/IJ1_plugin.java
+++ src/main/java/boa/gui/IJ1_plugin.java
@@ -17,46 +17,76 @@
  */
 package boa.gui;
 
+import static boa.gui.GUI.logger;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ij.plugin.PlugIn;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 import loci.common.DebugTools;
 import loci.formats.FormatHandler;
 import org.apache.log4j.LogManager;
+import org.scijava.ui.swing.options.OptionsLookAndFeel;
 import org.slf4j.LoggerFactory;
+import utils.Utils;
 
 /**
  *
  * @author jollion
  */
 public class IJ1_plugin implements PlugIn {
-    
+    @Override
     public void run(String string) {
+        
+        String lookAndFeel = null;
+        
+        Map<String, LookAndFeelInfo> lafMap = Arrays.asList(UIManager.getInstalledLookAndFeels()).stream().collect(Collectors.toMap(LookAndFeelInfo::getName, Function.identity()));
+        logger.info("LookAndFeels {}", lafMap.keySet());
+        if (lafMap.keySet().contains("Quaqua")) lookAndFeel="Quaqua";
+        else if (lafMap.keySet().contains("Seaglass")) lookAndFeel="Seaglass";
+        else if (lafMap.keySet().contains("Nimbus")) lookAndFeel="Nimbus";
+        else if (lafMap.keySet().contains("Metal")) lookAndFeel="Metal";
         /*
-        try {
-            // Set cross-platform Java L&F (also called "Metal")
-            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName());
-        } 
-        catch (UnsupportedLookAndFeelException e) {
-           // handle exception
+        String OS_NAME = System.getProperty("os.name");
+        if ("Linux".equals(OS_NAME)) {
+            if (uiNames.contains("Nimbus")) lookAndFeel="Nimbus";
+        }*/
+        if (lookAndFeel!=null) {
+            logger.info("set LookAndFeel: {}", lookAndFeel);
+            try {
+                // Set cross-platform Java L&F (also called "Metal")
+                UIManager.setLookAndFeel( lafMap.get(lookAndFeel).getClassName());
+            } 
+            catch (UnsupportedLookAndFeelException e) {
+               // handle exception
+            }
+            catch (ClassNotFoundException e) {
+               // handle exception
+            }
+            catch (InstantiationException e) {
+               // handle exception
+            }
+            catch (IllegalAccessException e) {
+               // handle exception
+            }
         }
-        catch (ClassNotFoundException e) {
-           // handle exception
-        }
-        catch (InstantiationException e) {
-           // handle exception
-        }
-        catch (IllegalAccessException e) {
-           // handle exception
-        }
+        
+        /*
+        
         */
         //System.setProperty("scijava.log.level", "error");
         //DebugTools.enableIJLogging(false);
         //DebugTools.enableLogging("ERROR");
         ((Logger)LoggerFactory.getLogger(FormatHandler.class)).setLevel(Level.OFF);
+        System.setProperty("scijava.log.level", "error");
+        
         
         // TODO find other IJ1&2 plugins & ops...
         
