3f8d15c61e59f33d03fdc2348b40aadfb3bdd58b
jeanollion <ollion@mnhn.fr>
2017-10-05 11:38:38 +0200

    fixed minor bugs

--- src/main/java/configuration/parameters/ParameterUtils.java
+++ src/main/java/configuration/parameters/ParameterUtils.java
@@ -218,6 +218,8 @@ public class ParameterUtils {
     }
 
     public static boolean arraysEqual(int[] array1, int[] array2) {
+        if (array1==null && array2==null) return true;
+        else if (array1==null || array2==null) return false;
         if (array1.length == array2.length) {
             for (int i = 0; i < array1.length; ++i) {
                 if (array1[i] != array2[i]) {
