7702e0888052e71c70b2f4cd5231df93951fe0c6
jeanollion <ollion@mnhn.fr>
2018-03-12 11:27:56 +0100

    update licence to GPL3

--- src/main/java/boa/image/ImageProperties.java
+++ src/main/java/boa/image/ImageProperties.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2018 Jean Ollion
+ *
+ * This File is part of BOA
+ *
+ * BOA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BOA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BOA.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package boa.image;
 
 public interface ImageProperties<T extends ImageProperties> extends BoundingBox<T> {
