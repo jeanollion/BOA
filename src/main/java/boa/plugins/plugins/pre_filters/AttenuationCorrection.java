a8e29e669ac8781ac0c4d461f6e10e7ac144b2de
jeanollion <ollion@mnhn.fr>
2018-03-12 12:27:54 +0100

    update licence to GPLv3

--- src/main/java/boa/plugins/plugins/pre_filters/AttenuationCorrection.java
+++ src/main/java/boa/plugins/plugins/pre_filters/AttenuationCorrection.java
@@ -1,6 +1,20 @@
-/*
- * from Philippe Andrey, Eric Biot, Souad Blila
-    http://imagejdocu.tudor.lu/doku.php?id=plugin:stacks:attenuation_correction:start
+/* 
+ * Copyright (C) 2018 Jean Ollion
+ *
+ * This File is part of BOA
+ *
+ * BOA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BOA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BOA.  If not, see <http://www.gnu.org/licenses/>.
  */
 package boa.plugins.plugins.pre_filters;
 import ij.IJ;
