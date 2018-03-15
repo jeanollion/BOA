f7ab1462feb518abc4f1800cd62552106681199e
jollion <jollion@jollion>
2018-03-15 19:39:44 +0100

    minor changes on GUI

--- src/main/java/boa/gui/imageInteraction/TrackMask.java
+++ src/main/java/boa/gui/imageInteraction/TrackMask.java
@@ -125,7 +125,7 @@ public abstract class TrackMask extends ImageObjectInterface {
     
     @Override public Image generatemage(final int structureIdx, boolean background) {
         // use track image only if parent is first element of track image
-        if (trackObjects[0].parent.getOffsetInTrackImage()!=null && trackObjects[0].parent.getOffsetInTrackImage().xMin()==0 && trackObjects[0].parent.getTrackImage(structureIdx)!=null) return trackObjects[0].parent.getTrackImage(structureIdx);
+        //if (trackObjects[0].parent.getOffsetInTrackImage()!=null && trackObjects[0].parent.getOffsetInTrackImage().xMin()==0 && trackObjects[0].parent.getTrackImage(structureIdx)!=null) return trackObjects[0].parent.getTrackImage(structureIdx);
         
         Image image0 = trackObjects[0].generatemage(structureIdx, false);
         if (image0==null) return null;
